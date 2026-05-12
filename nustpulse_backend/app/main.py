47aca9f04e09e374bf715840382d8ba5337b7f2d
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-05-12 12:26:09 +0500

    Production: Updated CORS for Vercel deployment

--- nustpulse_backend/app/main.py
+++ nustpulse_backend/app/main.py
@@ -6,13 +6,7 @@ app = FastAPI(title="NustPulse API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080", 
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:8000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
