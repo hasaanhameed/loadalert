9a0740be4af678c380d1404ffb499e32460191df
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-05-13 13:05:26 +0500

    Check: notification reschedule

--- nustpulse_backend/app/core/celery_app.py
+++ nustpulse_backend/app/core/celery_app.py
@@ -28,8 +28,8 @@ celery_app.conf.beat_schedule = {
         "task": "sync_all_users",
         "schedule": crontab(minute=0, hour="*/4"),
     },
-    "daily-reminders-at-1pm": {
+    "daily-reminders-at-110pm": {
         "task": "daily_reminder_check",
-        "schedule": crontab(minute=0, hour=13),
+        "schedule": crontab(minute=10, hour=13),
     },
 }
